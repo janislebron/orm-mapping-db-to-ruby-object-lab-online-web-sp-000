7c139c32033c5becce250971cd9546a954e7f0d7
Janis Lebron <janislebron@gmail.com>
2021-04-08 22:57:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
   attr_accessor :id, :name, :grade
 
-
+  
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
@@ -81,7 +81,7 @@ end
   end
 
 
-  def self.first_X_students_in_grade_10(name, grade)
+  def self.first_X_students_in_grade_10(x, grade)
     sql = <<-SQL
        SELECT *
        FROM students
@@ -89,7 +89,7 @@ end
        LIMIT ?
     SQL
 
-  DB[:conn].execute(sql, name, grade).map do |row|
+  DB[:conn].execute(sql, x, grade).map do |row|
     self.new_from_db(row)
   end.first
 end
